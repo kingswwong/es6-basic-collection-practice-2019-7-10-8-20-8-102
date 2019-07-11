3f5dd8ff0ce1ebf91768ae4028a78c231a74f71c
“Dean” <467271883@qq.com>
2019-07-11 13:30:08 +0800

    Add section2-practice-2 impletement

--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  let temp = collection.reduce((re, cur) => {
+    let letter = cur.split("-")[0];
+    let count = cur.split("-")[1] === undefined ? 1 : cur.split("-")[1]
+    re[letter] ? re[letter] += parseInt(count) : re[letter] = parseInt(count);
+    return re;
+  },{})
+  let keys = Object.keys(temp);
+  let result = [];
+  keys.forEach(key => {
+    result.push({key: key, count: temp[key]})
+  })
+  return result;
 }
