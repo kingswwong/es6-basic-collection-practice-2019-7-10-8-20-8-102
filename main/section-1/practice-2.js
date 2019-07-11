b96b490dbfc8fe4f3f3f710b0d1d19e5b1af55c0
“Dean” <467271883@qq.com>
2019-07-11 13:29:29 +0800

    Add section1-practoce-2 impletement

--- main/section-1/practice-2.js
+++ main/section-1/practice-2.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  let result = []
+  collectionB.forEach(cB => {
+    collectionA.forEach(cA => {
+      if(cB.indexOf(cA) !== -1){
+        result.push(cA)
+      }
+    })
+  })
+  return result.reduce((re, cur) => {
+    if(re.indexOf(cur) === -1){
+      re.push(cur)
+    }
+    return re;
+  },[])
 }
